c148e87bfa2dd8a3b2fb1c2854609b68023b1bcd
cofucan <coolucheofia@gmail.com>
2023-05-08 23:17:07 +0100

    fix: Include check for node directly after  pointer

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,10 +15,11 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
+	/* Check if list is null or contains only 1 node */
 	if (!list || !list->next)
 		return (0);
 
-	while (slow->next && fast->next->next)
+	while (slow->next && fast->next && fast->next->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
