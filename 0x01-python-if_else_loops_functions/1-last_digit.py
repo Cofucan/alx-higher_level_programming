15fc84593c1789b0066c011751e6cb76a8689af5
cofucan <coolucheofia@gmail.com>
2023-05-09 17:17:02 +0100

    style: Pycodestyle compliant

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,8 @@
 import random
 
 number = random.randint(-10000, 10000)
-abs(number) # Ensure that the number is of correct type.
-last_digit = int(list(str(number))[-1]) # Convert to list and get last num.
+abs(number)  # Ensure that the number is of correct type.
+last_digit = int(list(str(number))[-1])  # Convert to list and get last num.
 last_digit = -(last_digit) if number < 0 else last_digit
 
 print(f"Last digit of {number} is {last_digit} and is ", end="")
