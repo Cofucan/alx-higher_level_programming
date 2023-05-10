ae9da91357b56c253963fd22cb13542d63e4997c
cofucan <coolucheofia@gmail.com>
2023-05-10 02:02:31 +0100

    feat: Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for idx, char in enumerate("abcdefghijklmnopqrstuvwxyz"[::-1], 1):
-    if idx & 1 != 0:
-        print("{}".format(char), end="")
-    elif idx & 1 == 0:
-        print("{}".format(chr(ord(char) - 32)), end="")
+for char in "AbCdEfGhIjKlMnOpQrStUvWxYz"[::-1]:
+    print("{}".format(char), end="")
