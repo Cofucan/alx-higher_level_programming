374d73302196ba1b1e59a0d8caa411d538eecef5
cofucan <coolucheofia@gmail.com>
2023-05-11 19:07:15 +0100

    fix: Directly import module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import py_compile
-    import imp
+    import hidden_4
 
-    pyc_file = './hidden_4.pyc'
-    py_compile.compile(pyc_file)
-
-    hidden = imp.load_compiled('hidden', pyc_file)
-
-    names = [name for name in dir(hidden) if not name.startswith('__')]
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
 
     for name in sorted(names):
         print(name)
