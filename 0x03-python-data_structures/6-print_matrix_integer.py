6ca5e8bc4ebe32840dac4c8df09721ed7a563e3f
cofucan <coolucheofia@gmail.com>
2023-05-13 12:04:40 +0100

    feat: Function to print list of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for idx, col in enumerate(row):
+                if idx == (len(row) - 1):
+                    print("{:d}".format(col))
+                else:
+                    print("{:d}".format(col), end=" ")
