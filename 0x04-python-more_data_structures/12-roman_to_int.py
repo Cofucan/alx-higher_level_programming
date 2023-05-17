f05e5d86d2acf5cf2d94c1d9eb5240e81aeaf705
cofucan <coolucheofia@gmail.com>
2023-05-17 21:41:36 +0100

    feat: Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string is None:
+        return None
+
+    numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            }
+
+    if not any(c in numerals.keys() for c in roman_string):
+        return None
+
+    rnum = numerals[roman_string[0]]
+    for i, s in enumerate(roman_string[1:], 1):
+        if numerals[roman_string[i-1]] < numerals[roman_string[i]]:
+            rnum += numerals[roman_string[i]] - 2*(numerals[roman_string[i-1]])
+        else:
+            rnum += numerals[s]
+
+    return rnum
