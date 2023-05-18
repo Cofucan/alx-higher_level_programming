75963cc1d83952a0724cf3eaba8a860e00979ee6
cofucan <coolucheofia@gmail.com>
2023-05-18 01:15:57 +0100

    fix: Rewrite functions

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,8 +2,6 @@
 #include "/usr/include/python3.4/listobject.h"
 #include "/usr/include/python3.4/object.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
@@ -18,12 +16,12 @@ void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p)
 {
 	int i, size, alloc;
-	PyVarObject *pvar = (PyVarObject *)p;
-	PyListObject *plist = (PyListObject *)p;
 	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
-	size = pvar->ob_size;
-	alloc = plist->allocated;
+	size = var->ob_size;
+	alloc = list->allocated;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %d\n", size);
@@ -31,10 +29,10 @@ void print_python_list(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		type = plist->ob_item[i]->ob_type->tp_name;
+		type = list->ob_item[i]->ob_type->tp_name;
 		printf("Element %d: %s\n", i, type);
-		if (!strcmp(type, "bytes"))
-			print_python_bytes(plist->ob_item[i]);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
 	}
 }
 
@@ -47,7 +45,7 @@ void print_python_list(PyObject *p)
 
 void print_python_bytes(PyObject *p)
 {
-	unsigned char f, size;
+	unsigned char i, size;
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
@@ -66,10 +64,10 @@ void print_python_bytes(PyObject *p)
 		size = ((PyVarObject *)p)->ob_size + 1;
 
 	printf("  first %d bytes: ", size);
-	for (f = 0; f < size; f++)
+	for (i = 0; i < size; i++)
 	{
 		printf("%02hhx", bytes->ob_sval[i]);
-		if (f == (size - 1))
+		if (i == (size - 1))
 			printf("\n");
 		else
 			printf(" ");
