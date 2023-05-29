be647c90dda5de65759674253246db7828de504c
Uche Ofia <coolucheofia@gmail.com>
2023-05-29 14:58:26 +0100

    refactor: Create variable for integer casting

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        val = int(value)
+        print("{:d}".format(val))
     except Exception:
         return False
 
