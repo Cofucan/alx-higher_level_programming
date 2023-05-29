e49197b867b5b1781bb39340e522f012a3b726e4
Uche Ofia <coolucheofia@gmail.com>
2023-05-29 14:39:40 +0100

    style: Pycodestyle compliant

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for e in range(x):
         try:
             print(my_list[e], end="")
-            count +=  1
+            count += 1
         except IndexError:
             break
     print("")
