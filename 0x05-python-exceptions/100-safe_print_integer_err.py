3290fc88fe1ecfde2ff2d5ac8d1e84c78a9efbd1
Uche Ofia <coolucheofia@gmail.com>
2023-05-30 03:00:55 +0100

    fix: Handle exceptions generally

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as v:
-        sys.stderr.write(f"Exception: {v}\n")
+    except Exception as e:
+        sys.stderr.write(f"Exception: {e}\n")
         return False
