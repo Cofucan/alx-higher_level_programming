de75d81baba6324089d4ea436fb07d67e54251f9
Uche Ofia <coolucheofia@gmail.com>
2023-05-31 03:07:42 +0100

    feat: Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+    This module defines Square class with a private instance
+    attribute, including a default argument.
+"""
+
+
+class Square:
+    """ An class definition for a square. """
+    def __init__(self, size=0):
+        """Initialization of instance attributes.
+
+        Args:
+            size (int): The unit length of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter function for private variable size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter function for private variable size.
+
+        Args:
+            value (int): The unit length of the square.
+
+        Raises:
+            TypeError: If arg `value` is not an integer.
+            ValueError: If arg `value` is negative.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Returns the current square area. """
+
+        return self.__size**2
+
+
+def validate(value):
+    """ Raises error is `value` is not valid.
+
+    Args:
+        value (int): The unit length of the square.
+
+    Raises:
+        TypeError: If arg `value` is not an integer.
+        ValueError: If arg `value` is negative.
+    """
+    if type(value) is not int:
+        raise TypeError("size must be an integer")
+    elif value < 0:
+        raise ValueError("size must be >= 0")
