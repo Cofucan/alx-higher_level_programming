5572fee465bf4b486009dd2f3a0496cbc68f543d
Uche Ofia <coolucheofia@gmail.com>
2023-06-05 23:56:33 +0100

    docs: Update docstrings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Define a Rectangle class with width and height
+    Define a Rectangle class with width and height.
 """
 
 
@@ -19,25 +19,16 @@ class Rectangle:
 
     @property
     def width(self):
-        """width property getter: retrieve the wdith of the class.
-
-        Returns:
-            width (int): the width instance attribute
-
-        width.setter: set the width of the class.
-        it also checks for TypeError and ValueError.
-
-        Args:
-            value (int): value to set the width to
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0.
-        """
+        """ Getter function for private variable, width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ Setter function for private variable, width.
+
+        Args:
+            value (int): The new width to set.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -47,25 +38,16 @@ class Rectangle:
 
     @property
     def height(self):
-        """height property getter: retrieve the height of the class.
-
-        Returns:
-            height (int): the height instance attribute
-
-        height.setter: set the height of the class.
-        it also checks for TypeError and ValueError.
-
-        Args:
-            value (int): value to set the height to
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0.
-        """
+        """ Getter function for private variable, height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter function for private variable, height.
+
+        Args:
+            value (int): The new height to set.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
