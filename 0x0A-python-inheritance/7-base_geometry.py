4935ce69c8320f39efc8dd93da4c350c613764d9
Uche Ofia <coolucheofia@gmail.com>
2023-06-12 09:29:46 +0100

    revert: Revert previous change and use string formating instead of f-string

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-"""Define an empty BaseGeometry class"""
+"""Geometry module"""
 
 
 class BaseGeometry:
-    """An empty class"""
+    """Defines a BaseGeometry class."""
 
     def area(self):
-        """Raise an exception"""
+        """Computes the area of the geometry.
+
+        Raises:
+            Exception: Because the logic is not implemented yet.
+        """
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """Validates integer value
+    def integer_validator(self, name: str, value: int):
+        """Validates `value`.
 
         Args:
-            name (str): name of the value
-            value (int): the value passed
+            name (str): Name
+            value (int): Value
 
-         Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is <= 0
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is not a positive, non-zero integer.
         """
         if not isinstance(value, int):
+            # raise TypeError(f"{name} must be an integer")
             raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
+
+        if value <= 0:
+            # raise ValueError(f"{name} must be greater than 0")
             raise ValueError("{} must be greater than 0".format(name))
