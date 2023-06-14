54637b15cbe4ee182d8593f7a6b23bb73e344196
cofucan <coolucheofia@gmail.com>
2023-06-14 02:18:54 +0100

    fix: Only execute script when run directly

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,11 +6,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = list(sys.argv[1:])
-filename = "add_item.json"
+if __name__ == "__main__":
+    filename = "add_item.json"
+    args = list(sys.argv[1:])
 
-if os.path.isfile(filename):
-    from_file = list(load_from_json_file(filename))
-    args.extend(from_file)
+    if os.path.isfile(filename):
+        from_file = list(load_from_json_file(filename))
+        args.extend(from_file)
 
-save_to_json_file(args, filename)
+    save_to_json_file(args, filename)
