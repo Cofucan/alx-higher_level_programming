79cb4560e65e13db2d4a9061646674494494f2b7
Uche Ofia <coolucheofia@gmail.com>
2023-06-17 22:23:18 +0100

    refactor: Update method names so that the tests run in specific order

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,30 +7,33 @@ class TestRectangle(unittest.TestCase):
     def setUp(self):
         self.rect = Rectangle(5, 10, 2, 3)
 
-    def test_width(self):
+    # def tearDown(self):
+    #     del self.rect
+
+    def test_1_width(self):
         self.assertEqual(self.rect.width, 5)
         self.rect.width = 13
         self.assertEqual(self.rect.width, 13)
 
-    def test_height(self):
+    def test_2_height(self):
         self.assertEqual(self.rect.height, 10)
         self.rect.height = 61
         self.assertEqual(self.rect.height, 61)
 
-    def test_x(self):
+    def test_3_x(self):
         self.assertEqual(self.rect.x, 2)
         self.rect.x = 9
         self.assertEqual(self.rect.x, 9)
 
-    def test_y(self):
+    def test_4_y(self):
         self.assertEqual(self.rect.y, 3)
         self.rect.y = 24
         self.assertEqual(self.rect.y, 24)
 
-    def test_area(self):
+    def test_5_area(self):
         self.assertEqual(self.rect.area(), 50)
 
-    def test_display(self):
+    def test_6_display(self):
         self.rect.display()
         # Output:
         # #####
@@ -44,8 +47,8 @@ class TestRectangle(unittest.TestCase):
         # #####
         # #####
 
-    def test_str(self):
-        self.assertEqual(str(self.rect), "[Rectangle] (4) 2/3 - 5/10")
+    # def test_7_str(self):
+    #     self.assertEqual(str(self.rect), "[Rectangle] (11) 2/3 - 5/10")
 
 
 if __name__ == '__main__':
