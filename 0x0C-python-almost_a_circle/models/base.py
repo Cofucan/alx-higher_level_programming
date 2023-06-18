2710df2b2f8aa815259b3618d4ab167867a6fb27
Uche Ofia <coolucheofia@gmail.com>
2023-06-18 05:34:44 +0100

    docs: Add docstrings to Base class and methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ import os
 
 
 class Base:
+    """Base class which will be inherited by all other classes."""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -19,14 +20,35 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries) -> str:
+        """Serializes a list of dictionaries to JSON string.
+
+        Args:
+            list_dictionaries (list of dicts): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of the list of dictionaries.
+        """
         return json.dumps(list_dictionaries) if list_dictionaries else "[]"
 
     @staticmethod
-    def from_json_string(json_string) -> list:
+    def from_json_string(json_string) -> any:
+        """Deserializes a JSON string to Python object.
+
+        Args:
+            json_string (JSON str): The JSON string to deserialize.
+
+        Returns:
+            any: A Python object.
+        """
         return json.loads(json_string) if json_string else []
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of `list_objs` to a file.
+
+        Args:
+            list_objs (list of objs): List of objects to be written.
+        """
         filename = f"{cls.__name__}.json"
         with open(filename, mode="w", encoding="utf-8") as json_file:
             if not list_objs:
@@ -37,6 +59,14 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """Creates an instance with all attributes set.
+
+        Args:
+            dictionary: Dictionary of arguments to be used to create instance.
+
+        Returns:
+            object: An instance of a class that inherits from this class.
+        """
         if cls.__name__ == "Rectangle":
             dummy = cls(8, 4)
 
@@ -47,7 +77,12 @@ class Base:
         return dummy
 
     @classmethod
-    def load_from_file(cls):
+    def load_from_file(cls) -> list:
+        """Reads data from a JSON file and creates instances with the data.
+
+        Returns:
+            list: A list of instances created using the read data.
+        """
         filename = f"{cls.__name__}.json"
         if not os.path.isfile(filename):
             return []
@@ -59,6 +94,11 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
+        """Serializes a list of objects to a CSV file.
+
+        Args:
+            list_objs (list): List of objects whose data is to be saved to CSV.
+        """
         filename = f"{cls.__name__}.csv"
         with open(filename, mode="w", encoding="utf-8") as csv_file:
             if cls.__name__ == "Rectangle":
@@ -70,7 +110,12 @@ class Base:
             writer.writerows([obj.to_dictionary() for obj in list_objs])
 
     @classmethod
-    def load_from_file_csv(cls):
+    def load_from_file_csv(cls) -> list:
+        """Deserializes a CSV file and uses the data to create new objects.
+
+        Returns:
+            list: List of objects created with the data read from the CSV.
+        """
         filename = f"{cls.__name__}.csv"
 
         try:
