8d2b90cdbecbe32a3b58da1976e2d30f4ce1efa8
cofucan <coolucheofia@gmail.com>
2023-06-18 02:14:37 +0100

    feat: Implement save_to_file() method in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,13 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries) -> str:
         return json.dumps(list_dictionaries) if list_dictionaries else "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = f"{cls.__name__}.json"
+        with open(filename, mode="w", encoding="utf-8") as s:
+            if not list_objs:
+                s.write(cls.to_json_string([]))
+            else:
+                dict_objs = [obj.to_dictionary() for obj in list_objs]
+                s.write(cls.to_json_string(dict_objs))
