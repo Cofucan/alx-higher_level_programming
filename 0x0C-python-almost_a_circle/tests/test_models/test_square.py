3eaaa0d591e77967041a90f619bcf7e0365b888a
cofucan <coolucheofia@gmail.com>
2023-06-20 02:30:38 +0100

    test: Implement many test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,67 +1,107 @@
 #!/usr/bin/python3
-
+"""
+A module that test differents behaviors
+of the Square class
+"""
 import unittest
+from models.base import Base
 from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-    """Test cases for the Square class."""
+    """Unit tests for testing the Square class."""
 
-    def setUp(self):
-        """Set up the test fixture."""
-        self.square = Square(5, 1, 2, 100)
+    def test_getter(self):
+        """Test the getter method of Square."""
+        square = Square(5)
+        self.assertEqual(square.size, 5)
 
-    def test_constructor(self):
-        """Test the constructor of the Square class."""
-        self.assertEqual(self.square.size, 5)
-        self.assertEqual(self.square.width, 5)
-        self.assertEqual(self.square.height, 5)
-        self.assertEqual(self.square.x, 1)
-        self.assertEqual(self.square.y, 2)
-        self.assertEqual(self.square.id, 100)
+    def test_setter(self):
+        """Test the setter method of Square."""
+        square = Square(5)
+        square.size = 8
+        self.assertEqual(square.size, 8)
 
-    def test_set_size(self):
-        """Test setting the size of the square."""
-        self.square.size = 8
-        self.assertEqual(self.square.size, 8)
-        self.assertEqual(self.square.width, 8)
-        self.assertEqual(self.square.height, 8)
+    def test_string(self):
+        """Test setting size with a string value."""
+        square = Square(3)
+        with self.assertRaises(TypeError):
+            square.size = "Hi"
 
-    def test_update_with_args(self):
-        """Test updating the square's attributes with positional arguments."""
-        self.square.update(200, 7, 2, 3)
-        self.assertEqual(self.square.size, 7)
-        self.assertEqual(self.square.width, 7)
-        self.assertEqual(self.square.height, 7)
-        self.assertEqual(self.square.x, 2)
-        self.assertEqual(self.square.y, 3)
-        self.assertEqual(self.square.id, 200)
+    def test_negative(self):
+        """Test setting size with a negative value."""
+        square = Square(6)
+        with self.assertRaises(ValueError):
+            square.size = -5
 
-    def test_update_with_kwargs(self):
-        """Test updating the square's attributes with keyword arguments."""
-        self.square.update(size=8, x=3, y=4, id=300)
-        self.assertEqual(self.square.size, 8)
-        self.assertEqual(self.square.width, 8)
-        self.assertEqual(self.square.height, 8)
-        self.assertEqual(self.square.x, 3)
-        self.assertEqual(self.square.y, 4)
-        self.assertEqual(self.square.id, 300)
+    def test_zero(self):
+        """Test setting size with zero."""
+        square = Square(6)
+        with self.assertRaises(ValueError):
+            square.size = 0
+
+    def test_decimal(self):
+        """Test setting size with a decimal value."""
+        square = Square(6)
+        with self.assertRaises(TypeError):
+            square.size = 1.5
+
+    def test_tuple(self):
+        """Test setting size with a tuple value."""
+        square = Square(7)
+        with self.assertRaises(TypeError):
+            square.size = (2, 8)
+
+    def test_empty(self):
+        """Test setting size with an empty value."""
+        square = Square(7)
+        with self.assertRaises(TypeError):
+            square.size = ""
+
+    def test_none(self):
+        """Test setting size with None."""
+        square = Square(5)
+        with self.assertRaises(TypeError):
+            square.size = None
+
+    def test_list(self):
+        """Test setting size with a list value."""
+        square = Square(4)
+        with self.assertRaises(TypeError):
+            square.size = [4, 7]
+
+    def test_dict(self):
+        """Test setting size with a dictionary value."""
+        square = Square(5)
+        with self.assertRaises(TypeError):
+            square.size = {"hi": 5, "world": 8}
+
+    def test_width(self):
+        """Test if width and height are updated when setting size."""
+        square = Square(5)
+        square.size = 6
+        self.assertEqual(square.width, 6)
+        self.assertEqual(square.height, 6)
 
     def test_to_dictionary(self):
-        """Test converting the square to a dictionary."""
-        expected_dict = {
-            "id": 100,
-            "size": 5,
-            "x": 1,
-            "y": 2
-        }
-        self.assertEqual(self.square.to_dictionary(), expected_dict)
+        """Test the to_dictionary method of Square."""
+        Base._Base__nb_objects = 0
 
-    def test_str(self):
-        """Test the string representation of the square."""
-        expected_str = "[Square] (100) 1/2 - 5"
-        self.assertEqual(str(self.square), expected_str)
+        square = Square(10, 2, 1, 9)
+        square_dict = square.to_dictionary()
+        expected = {"id": 9, "x": 2, "size": 10, "y": 1}
+        self.assertEqual(square_dict, expected)
+
+        square = Square(1, 0, 0, 9)
+        square_dict = square.to_dictionary()
+        expected = {"id": 9, "x": 0, "size": 1, "y": 0}
+        self.assertEqual(square_dict, expected)
+
+        square.update(5, 5, 5, 5)
+        square_dict = square.to_dictionary()
+        expected = {"id": 5, "x": 5, "size": 5, "y": 5}
+        self.assertEqual(square_dict, expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
