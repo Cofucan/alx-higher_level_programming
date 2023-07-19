037847fcd62415df2f4b02a1f1e88e80919dae5a
Uche Ofia <coolucheofia@gmail.com>
2023-07-19 21:38:18 +0100

    fix: Use proper keywword

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
--- SQL script that creates database and table
--- with unique, auto generated, non null,
--- primary key id column
+-- SQL script that creates database and table with unique
+-- auto generated, non null, primary key id column
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT NON NULL AUTO INCREMENT,
-    name VARCHAR(256),
-    PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
