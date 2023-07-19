0464b0ed09f07e7a928512144e954b8a634b299f
Uche Ofia <coolucheofia@gmail.com>
2023-07-19 18:40:53 +0100

    fix: Correct syntax for granting priviledges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- creates a new user with SELECT priviledges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
