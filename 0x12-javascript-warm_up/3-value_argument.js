62c4fca5d46331c1ec00313ba1c22434ba202245
Uche Ofia <coolucheofia@gmail.com>
2023-08-14 20:06:25 +0100

    fix: Exclude the use of

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
-const argLen = argv.length;
 
-if (argLen === 2) {
+if (!argv[2]) {
   console.log('No argument');
-} else if (argLen > 2) {
+} else {
   console.log(argv[2]);
 }
