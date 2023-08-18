b7ff9aadc807b1084870ac8ce1ab6154f9440ca9
Uche Ofia <coolucheofia@gmail.com>
2023-08-18 12:05:13 +0100

    fix: Code does not execute when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,12 +11,14 @@ host: str = "localhost"
 port: int = 3306
 statement: str = """SELECT * FROM states ORDER BY id"""
 
-db = MySQLdb.connect(
-    user=username, host=host, port=port, password=password, database=db_name
-)
-cursor = db.cursor()
 
-cursor.execute(statement)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=username, host=host, port=port, password=password, database=db_name
+    )
+    cursor = db.cursor()
+
+    cursor.execute(statement)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
