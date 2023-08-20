17cf16a258551fb96ee8b6a8fb684086b238defc
Uche Ofia <coolucheofia@gmail.com>
2023-08-20 22:51:02 +0100

    wip

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,10 +6,11 @@ contained in the database hbtn_0e_101_usa
 
 import sys
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
     username: str = sys.argv[1]
@@ -20,15 +21,21 @@ if __name__ == "__main__":
 
     Session = sessionmaker()
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@{host}/{db_name}',
+        f"mysql+mysqldb://{username}:{password}@{host}/{db_name}",
         pool_pre_ping=True,
     )
     Base.metadata.create_all(engine)
     Session.configure(bind=engine)
     session = Session()
 
-    if states := session.query(State).order_by(State.id):
+    # if states := session.query(State).order_by(State.id):
+    if (
+        states := session.query(State)
+        .outerjoin(City)
+        .order_by(State.id, City.id)
+        .all()
+    ):
         for state in states:
-            print(f'{state.id}: {state.name}')
+            print(f"{state.id}: {state.name}")
             for city in state.cities:
-                print(f'\t{city.id}: {city.name}')
+                print(f"\t{city.id}: {city.name}")
