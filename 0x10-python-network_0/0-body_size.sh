7e75c0a02eaf0ae9d7867c17a9227fa66bbe50a7
Uche Ofia <coolucheofia@gmail.com>
2023-09-01 00:37:36 +0100

    refactor: Use  and  to get the size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script akes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -w '%{size_download}\n' "$1" | tail -n 1
+curl -sI "$1" | grep Content-Length | cut -d " " -f 2
