3d0fc3264e50c58c1cddfa35678abcd41ac45e42
Uche Ofia <coolucheofia@gmail.com>
2023-09-01 00:31:34 +0100

    fix: Edit script to be exactly 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script akes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-url="$1"
-
-curl -s -w '%{size_download}\n' "$url" | tail -n 1
+curl -s -w '%{size_download}\n' "$1" | tail -n 1
