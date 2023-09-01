03c72af1f276176833d664a03ee6dea91adf51a9
Uche Ofia <coolucheofia@gmail.com>
2023-09-01 01:38:37 +0100

    fix: Use proper flag for sending data

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script takes in a URL and sends a POST request to the passed URL, and displays the body of the response
-curl -s -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -X POST "$1"
+# It also sends custom headers along with the request.
+curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -X POST "$1"
