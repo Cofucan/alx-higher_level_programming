08324e4fb73355f30364d38599aa580d1c28e306
Uche Ofia <coolucheofia@gmail.com>
2023-09-01 01:38:14 +0100

    docs: Update script descriptions

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script takes in a URL and sends a GET request to the URL, and displays the body of the response
+# It also sends custom headers along with the request.
 curl -s -H "X-School-User-Id: 98" -X GET "$1"
