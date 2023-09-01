6666c1f5d2722b9393482463d5c40ce16dc508f9
Uche Ofia <coolucheofia@gmail.com>
2023-09-01 01:41:38 +0100

    fix: Remove comments that make script more than 3 lines

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and sends a GET request to the URL, and displays the body of the response
-# It also sends custom headers along with the request.
 curl -s -H "X-School-User-Id: 98" -X GET "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and sends a POST request to the passed URL, and displays the body of the response
-# It also sends custom headers along with the request.
 curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -X POST "$1"
